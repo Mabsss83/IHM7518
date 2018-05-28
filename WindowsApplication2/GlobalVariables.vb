8ad7095484aee53d6a64ff719d1db086bfea14bf
Mabsss83 <39689126+Mabsss83@users.noreply.github.com>
2018-05-28 11:00:58 +0200

    connection fix

--- WindowsApplication2/GlobalVariables.vb
+++ WindowsApplication2/GlobalVariables.vb
@@ -1,4 +1,4 @@
 ﻿Module GlobalVariables
-    Private ConnStr = "Server = " & Read_XML_Value("server") & "; Database=" & Read_XML_Value("db") & "; Uid=" & Read_XML_Value("user") & "; Pwd=" & Read_XML_Value("pwd") & ";"
+    Private ConnStr = "Server = " & Read_XML_Value("server") & "; Database=" & Read_XML_Value("db") & "; Uid=" & Read_XML_Value("user") & "; Pwd=" & Read_XML_Value("pwd") & "; SslMode=none ;"
     Public bdd As New BDD_Network(ConnStr)
 End Module
